711f36762c4ce67b80bc39454de20ff2055291ab
andy <andy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-25 14:50:13 +0000

    mrss: bump to 1.1.4. Add whitespace before xmlns to prevent XML parse error when used with other plugins that add xmlns without whitespace.
    git-svn-id: https://plugins.svn.wordpress.org/mrss/trunk@129406 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mrss.php
+++ mrss.php
@@ -3,7 +3,7 @@
 Plugin Name: MediaRSS
 Plugin URI: http://wordpress.org/extend/plugins/mrss/
 Description: Adds &lt;media&gt; tags to your feeds.
-Version: 1.1.3
+Version: 1.1.4
 Author: Andy Skelton
 Author URI: http://andy.wordpress.com/
 
@@ -28,7 +28,9 @@ function mrss_init() {
 }
 
 function mrss_ns() {
-	?>xmlns:media="http://search.yahoo.com/mrss/"
+	?>
+
+	xmlns:media="http://search.yahoo.com/mrss/"
 	<?php
 }
 
